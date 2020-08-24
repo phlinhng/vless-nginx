71e4402f3892978123ac059b1d5381deb9518494
phlinhng <phlinhng@outlook.com>
2020-08-25 02:51:08 +0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -164,7 +164,7 @@ server {
     listen 80 default_server;
     listen [::]:80 default_server;
     server_name _;
-    return 301 https://$host$request_uri;
+    return 301 https://\$host\$request_uri;
 }
 EOF
 }
